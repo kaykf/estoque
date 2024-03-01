48f301b0c2db2f4c0e37e4957bc7072b113b885f
kaykf <kaykfelipe071@gmail.com>
2024-03-01 15:52:58 -0300

    crud produtos completa

--- estoque/src/main/java/com/example/estoque/controller/ProdutosController.java
+++ estoque/src/main/java/com/example/estoque/controller/ProdutosController.java
@@ -2,11 +2,17 @@ package com.example.estoque.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +38,24 @@ public class ProdutosController {
         Produtos produtos = repository.findById(id).orElse(null);
         return produtos != null ? ResponseEntity.ok().body(produtos):
         ResponseEntity.notFound().build();
+    }
+     @PostMapping
+    public ResponseEntity<Produtos> criarProdutos(@RequestBody Produtos entity){
+        Produtos produtos = repository.save(entity); 
+        return ResponseEntity.status(HttpStatus.CREATED).body(produtos);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Produtos> editarProdutos(@PathVariable Long id,@RequestBody Produtos entity){
+        Produtos produtoSalvo = repository.findById(id).orElse(null);
+        if(produtoSalvo == null){
+            return ResponseEntity.notFound().build();
+        }
+        BeanUtils.copyProperties(entity, produtoSalvo,"id");
+        return ResponseEntity.ok().body(repository.save(produtoSalvo));
+    }
+     @DeleteMapping("/{id}")
+    public ResponseEntity<Produtos> delete(@PathVariable Long id){
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
